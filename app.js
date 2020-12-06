4569683ad8b6824b3560bfb267cfbeacec0779a0
Trevor Whitaker <trevor.whitaker@ibm.com@Trevors-MacBook-Pro.local>
2020-12-06 18:16:13 -0500

    Added auth stuff

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 
-const express = require('express')
+const express = require('express');
+const mongoose = require('mongoose');
 const path = require('path');
+require('dotenv').config();
 
 const app = express()
 
 app.use(express.static(path.join(__dirname, 'client-src', 'build')));
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -22,3 +25,14 @@ app.get('/home', function (req, res) {
 app.listen(process.env.PORT || 8080, () => {
   console.log('Server started on ', process.env.PORT || '8080',)
 });
+
+mongoose.connect(process.env.MONGODB_CONN_STRING, {
+  useNewUrlParser: true, 
+  useUnifiedTopology: true,
+  useCreateIndex: true
+}, (err) => {
+  if (err) throw error;
+  console.log("Mongodb connected");
+});
+
+app.use("/users", require("./routes/userRouter"));
