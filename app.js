c851f3e079c79d1561b1b043eeb40955d076dda2
Nick Gong <gptt916@hotmail.com>
2020-12-06 13:19:43 -0500

    Add test endpoint back

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'client-src', 'build', 'index.html'));
 })
 
+app.get('/test', (req, res) => {
+  res.json({poop: 'poop'})
+})
+
 app.listen(process.env.PORT || 8080, () => {
   console.log('Server started on ', process.env.PORT || '8080',)
 });
